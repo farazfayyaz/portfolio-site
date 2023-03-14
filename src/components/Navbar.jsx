fd68b84fbe0d7afe07ed6aa4ef5a60954d55685f
Faraz Fayyaz <113796246+farazfayyaz@users.noreply.github.com>
2023-03-14 13:22:20 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#303030]">
                         <a  className="flex justify-between items-center w-full text-gray-300"
-                        href="../assets/FarazFayyaz.pdf" >Resume</a>
+                        href="./assets/FarazFayyaz.pdf" >Resume</a>
                         <BsFillPersonLinesFill size={30}/>
                     </li>
                 </ul>
