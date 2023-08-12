321702cb081147031f527d629a6f74e0bbb30cda
Faraz Fayyaz <113796246+farazfayyaz@users.noreply.github.com>
2023-08-12 12:56:56 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,12 +16,12 @@ const Home = () => {
 
           <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Faraz Fayyaz</h1>
 
-          <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Frontend Developer.</h2>
+          <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full-Stack Software Engineer.</h2>
 
           <p className="text-[#8892b0] py-4 max-w-[700px]">
-            I am a Frontend Developer with a strong passion for 
+            I am a Full-Stack Software Engineer with a strong passion for 
             developing, designing, and creating digital experiences. Currently, 
-            I am focused on becoming a full-stack software engineer.
+            interested in AI and emerging technologies.
           </p>
 
           <div>
